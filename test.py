36e4b8665ebb2414b35349b5edc41f360f84ef8d
blackstar2029 <punyawat2312@gmail.com>
2025-07-01 11:13:07 +0700

    Add filtering

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from obspy import read
 
 st = read()
-st.plot()
+#st.plot()
 st[1].plot()
+st[1].spectrogram(log=True) #create spectrogram
+
+#Filter the wavefrom, tr -> trace (line)
+tr = st[1].copy()
+tr.filter('bandpass', freqmin = 1.0, freqmax = 5.0, corners = 4, zerophase = True)
+tr.plot()
+
+tr.write('demo_eq.mseed', format='MSEED')
+
+fig = tr.plot(show=True)
+fig.savefig('demo_Waveform.png', dpi=300)
